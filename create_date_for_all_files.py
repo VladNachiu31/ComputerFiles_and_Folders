63aa113d650d2a39956748f7a4611d1c77bd77a3
VladNachiu31 <nachiu.vlad@yahoo.com>
2023-03-06 12:43:44 +0000

    new file:   .idea/.gitignore 	new file:   .idea/ComputerFiles_and_Folders.iml 	new file:   .idea/dbnavigator.xml 	new file:   .idea/inspectionProfiles/Project_Default.xml 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   add_prefix.py 	new file:   change_extension_allfiles.py 	new file:   create_archive_for_files.py 	new file:   create_date_for_all_files.py 	new file:   create_empty_file.py 	new file:   destroy_files.py 	new file:   extract_path.py 	new file:   extract_zip_files.py 	new file:   files/files-abc.txt 	new file:   files/files-def.txt 	new file:   files/files-ghi.txt 	new file:   files1/February/2023-02-24_15_54_16_files-def.txt 	new file:   files1/February/2023-02-24_15_54_22_files-ghi.txt 	new file:   files1/January/2023-02-24_15_53_26_files-abc.txt 	new file:   rename_all_files_from_folder.py 	new file:   requirements.txt 	new file:   search_after_name.py

---
+++ create_date_for_all_files.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from _datetime import datetime
+
+
+root_dir = Path('files1')
+
+for path in root_dir.glob("**/*"):
+    if path.is_file():
+        created_date = datetime.fromtimestamp(path.stat().st_ctime)
+        created_date_str = created_date.strftime("%Y-%m-%d_%H:%M:%S")
+        new_filename = created_date_str + '_' + path.name
+        new_filepath = path.with_name(new_filename)
+        path.rename(new_filepath)
